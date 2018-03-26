1ba14c40bfb2f0d6a2bf8fb9185aa61af7aab309
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-03-26 16:51:55 +0100

    challenge 05 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,14 @@ describe Oystercard do
   it "has a balance" do
     expect(oystercard.balance).to be
   end
+
+  it "responds to top_up" do
+    expect(oystercard).to respond_to(:top_up)
+  end
+
+  it "adds funds when topping up" do
+    oystercard.top_up(10)
+    expect(oystercard.balance).to eq 10
+  end
+
 end
